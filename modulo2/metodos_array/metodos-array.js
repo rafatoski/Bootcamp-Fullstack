a03efee381a9276e832ff8069708df67ae787a1a
rafatoski <rafatoski@hotmail.com>
2024-08-08 20:41:42 -0500

    filter

--- modulo2/metodos_array/metodos-array.js
+++ modulo2/metodos_array/metodos-array.js
@@ -9,3 +9,29 @@ let nombres = personas.map(function(persona){
 });
 
 console.log(nombres);
+
+/* filtrar los numeros que sean pares  */
+
+let numeros = [1,2,3,4,5,6];
+let pares = numeros.filter(function(numero){
+    return numero % 2 === 0;
+});
+
+console.log(pares);
+
+/* filtro palabras cortas  */
+let palabras = ['Rafael','Carlos' , 'Manuela' , 'Mauro']
+let palabrasMin = palabras.filter(function(palabra){
+    return palabra.length <= 4;
+});
+
+console.log(palabrasMin);
+
+
+/* Sumar todos los numeros que se encuentran dentro del array  */
+let numeros2 = [1,2,3,4,5,6];
+let suma = numeros.reduce(function(acumulador, numero){
+    return acumulador + numero;
+}, 0);
+
+console.log(suma);
