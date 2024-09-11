8d204ed6a9fc70b96d75c0b0d7f8940a0c19abd3
rafatoski <rafatoski@hotmail.com>
2024-09-10 21:12:35 -0500

    complete

--- modulo3/node-js/react/ejercicio-2/contador/src/App.js
+++ modulo3/node-js/react/ejercicio-2/contador/src/App.js
@@ -1,22 +1,28 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/counter';
 
 function App() {
+  const [rotationDuration, setRotationDuration] = useState(20);
+
+  const handleCountChange  = (newCount) => {
+    // Comienza con 20 segundos y disminuye exponencialmente
+    const newDuration = Math.max(0.1, 20 / Math.pow(1.2, newCount));
+    setRotationDuration(newDuration);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        {/* Manten el logo girando*/}
+        <img src={logo} 
+        className='App-logo' 
+        alt='logo' 
+        style={{ '--rotation-duration': `${rotationDuration}s`}} 
+        />
+        <h1 className='App-tittle'>Interactive Counter</h1>
+        <Counter onCountChange={handleCountChange} />
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
       </header>
     </div>
   );
--- modulo3/node-js/react/ejercicio-2/contador/src/components/counter.jsx
+++ modulo3/node-js/react/ejercicio-2/contador/src/components/counter.jsx
@@ -1,4 +1,31 @@
-import React,{useState} from 'react'; 
-const Counter =()=> {
-    const[count,setCount]= useState(0);
+import React, { useState } from 'react';
+const Counter = ({ onCountChange }) => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        const newCount = count +1;
+        setCount(newCount);
+        onCountChange(newCount);
+    };
+    const decrement = () => {
+        const newCount = Math.max(0, count -1);
+        setCount(newCount);
+        onCountChange(newCount);
+    };
+    const reset = () => {
+        setCount(0);
+        onCountChange(0);
+    };
+
+    return (
+        <div className="calculator-container">
+            <div className="counter-screen">{count}</div>
+            <div className="buttons">
+                <button onClick={increment}>Increase ➕</button>
+                <button onClick={decrement}>Decrease ➖</button>
+                <button onClick={reset}>Reset 🔁 </button>
+            </div>
+        </div>
+    );
 };
+export default Counter;
