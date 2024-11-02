a2e62030585706d6e0bb14a76ae2f47f95e9e49f
rafatoski <rafatoski@hotmail.com>
2024-11-02 13:02:27 -0500

    mongo db

---
+++ modulo4/src/models/user.model.js
@@ -0,0 +1,17 @@
+import { type } from "express/lib/response";
+import mongoose from "mongoose";
+
+mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
