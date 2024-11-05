4d1ae6f5ab48c1ca5dc083ab0f4ce3aae65bcda7
rafatoski <rafatoski@hotmail.com>
2024-11-05 00:14:57 -0500

    Validar Token

--- modulo4/src/controllers/auth.controller.js
+++ modulo4/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken'
+import { createAccessToken } from '../libs/jwt.js';
 
 
 export const register = async (req, res) => {
@@ -15,24 +15,56 @@ export const register = async (req, res) => {
             password: passwordHash,
         })
 
-        const userSaved = await newUser.save()
+        const userSaved = await newUser.save();
+        const token = await createAccessToken({id: userSaved._id});
 
         res.cookie('token',token);
+        
         res.json({
-            message: 'usuario registrado satisfactoriamente',
-        })
-
-        // res.json({
-        //     id: userSaved._id,
-        //     username: userSaved.username,
-        //     email: userSaved.email,
-        //     createdAt: userSaved.createdAt,
-        //     updatedAt: userSaved.updatedAt,
-        // })
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt,
+        });
 
     } catch (error) {
-        console.log(error);
+        res.status(500).json({ message: error.message });
     }
 }
 
-export const login = (req, res) => res.send('login')
+export const login = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const userFound = await User.findOne({email})
+        if (!userFound) return res.status(400).json({message: "User not found"});
+
+        const isMatch = await bcrypt.compare(password, userFound.password);
+
+        if (!isMatch) return res.status(400).json({ message: "Incorrect Password"});
+        
+        const token = await createAccessToken({id: userFound._id});
+
+        res.cookie('token',token);
+        
+        res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt,
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+
+export const logout = (req, res) => {
+    res.cookie('token' , "", {
+            expires: new Date(0)
+        })
+        return res.sendStatus(200);
+}
